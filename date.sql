649a6e9a94dc1a6657ec8b20ecd249af8c9b1d62
touhidcodes <touhidcodes@gmail.com>
2024-03-12 11:23:31 +0600

    add: sql practice

---
+++ date.sql
@@ -0,0 +1,8 @@
+-- Active: 1709927137429@@127.0.0.1@5432@postgres_practice
+-- SHOW timezone
+
+-- SELECT now()
+
+SELECT to_char(now(), 'dd')
+
+SELECT age (CURRENT_DATE, '1999-12-27');
---
+++ groupBy.sql
@@ -0,0 +1,1 @@
+-- Active: 1709927137429@@127.0.0.1@5432@postgres_practice
